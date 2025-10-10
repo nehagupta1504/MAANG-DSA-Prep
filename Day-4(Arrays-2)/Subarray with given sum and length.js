fbf7db92c3d92a5eeefb573edff412d9e970f49f
Neha Gupta <nehagupta@Nehas-MacBook-Pro.local>
2025-10-10 19:25:38 +0530

    Day-4 subarray with given sum

--- Day-4(Arrays-2)/Subarray with given sum and length.js
+++ Day-4(Arrays-2)/Subarray with given sum and length.js
@@ -65,3 +65,38 @@
     Explanation 2:
     There are no such subarray.
 */
+
+
+function subArrayWithGivenSum(A, B, C){
+    let arr = findSumOfSubArraySizeB(A, B);
+    for(let i=0; i< arr.length; i++){
+        if(arr[i] == C){
+            return 1;
+        }
+    }
+    return 0;
+}
+
+function findSumOfSubArraySizeB(arr, window){
+    let opArray = [];
+    let sum = 0;
+    for(let i = 0; i< arr.length; i++){
+        if(i<window-1){
+            sum += arr[i]
+        }else{
+            sum += arr[i];
+            if(i-window<0){
+                opArray.push(sum);
+            }else{
+                sum -= arr[i-window];
+                opArray.push(sum);
+            }
+        }
+    }
+    return opArray;
+}
+
+console.log(subArrayWithGivenSum([4, 2, 2, 5, 1], 4, 6))
+
+// Time Complexity - O(n)
+// Space Complexity - O(k) -  where k is the window size
