05626234bfb57d57b962393335f0811486b22319
Neha Gupta <nehagupta150498@gmail.com>
2026-07-01 15:38:57 +0530

    added find k closest elements

--- Day-50 (2 Pointers)/Find K Closest Elements.js
+++ Day-50 (2 Pointers)/Find K Closest Elements.js
@@ -31,7 +31,7 @@
         -104 <= arr[i], x <= 104
 */
 
-
+// Note -  There can be various solutions for this problem but the best would be BS as the array given is sorted
 /*
 # Intuition
     1. Find the place of x in the array using binary search
@@ -107,4 +107,140 @@ function findPosInArray(arr, x){
 # Complexity Analysis
     TC:  O(logn + k)
         Explanation: Binary Search (findPosInArray()) O(log n)  + two-pointer pick // O(k) + reverse/merge    // O(k)
+*/
+
+/* 
+# Inutition
+    1. Maintain a max heap with max k size
+    2. the heap should contain max diff value at top
+    3. If the diff for 2 values are same then top should contain one with max value as it is easy to remove that way
+
+NOTE -This code is not completely optimised, the better way is to look at the code and think what optimisations you can make
+
+*/
+
+// Solution II (Heap)
+/**
+ * @param {number[]} arr
+ * @param {number} k
+ * @param {number} x
+ * @return {number[]}
+ */
+class Pair {
+    constructor(val, diff) {
+        this.val = val;
+        this.diff = diff;
+    }
+}
+class HeapImplementation {
+    constructor(k) {
+        this.heap = [];
+        this.k = k;
+    }
+    extractMax() {
+        if (this.size() === 0) return null;
+        this.swap(0, this.size() - 1);
+        let val = this.heap.pop();
+        this.heapifyDown(0);
+        return val;
+    }
+    getLeftChild(index) {
+        return 2 * index + 1;
+    }
+    getRightChild(index) {
+        return 2 * index + 2;
+    }
+    getParent(index) {
+        return Math.floor((index - 1) / 2);
+    }
+    heapifyDown(index) {
+        let leftChild = this.getLeftChild(index);
+        let largest = index;
+
+        if (leftChild >= this.size()) return;
+
+        if (
+            this.heap[leftChild].diff > this.heap[largest].diff ||
+            (
+                this.heap[leftChild].diff == this.heap[largest].diff &&
+                this.heap[leftChild].val > this.heap[largest].val
+            )
+        ) {
+            largest = leftChild;
+        }
+        let rightChild = this.getRightChild(index);
+        if (
+            rightChild < this.size() &&
+            (
+                this.heap[rightChild].diff > this.heap[largest].diff ||
+                (
+                    this.heap[rightChild].diff == this.heap[largest].diff &&
+                    this.heap[rightChild].val > this.heap[largest].val
+                )
+            )
+        ) {
+            largest = rightChild;
+        }
+        if (index != largest) {
+            this.swap(index, largest);
+            this.heapifyDown(largest);
+        }
+    }
+    heapifyUp(index) {
+        if (index == 0) {
+            return;
+        }
+        let parent = this.getParent(index);
+        if (
+            this.heap[parent].diff < this.heap[index].diff ||
+            (
+                this.heap[parent].diff == this.heap[index].diff &&
+                this.heap[parent].val < this.heap[index].val
+            )
+
+        ) {
+            this.swap(parent, index);
+            this.heapifyUp(parent);
+        }
+    }
+    insert(val, diff) {
+        let pair = new Pair(val, diff);
+        this.heap.push(pair);
+        this.heapifyUp(this.size() - 1);
+        if (this.size() > this.k) {
+            this.extractMax();
+        }
+    }
+    size() {
+        return this.heap.length;
+    }
+    swap(i, j) {
+        [this.heap[i], this.heap[j]] = [this.heap[j], this.heap[i]];
+    }
+    peek() {
+        return this.heap[0];
+    }
+
+}
+var findClosestElements = function (arr, k, x) {
+    let heap = new HeapImplementation(k);
+    for(let i=0; i<arr.length; i++){
+        heap.insert(arr[i], Math.abs(arr[i]-x));
+    }
+    let ans = [];
+    while(heap.size()){
+        let max = heap.extractMax();
+        ans.push(max.val);
+    }
+
+    ans.sort((a, b) => a - b);
+
+    return ans;
+}
+
+
+/*
+# Complexity Analysis- Heap
+    TC: O(n log k + k log k)
+    SC: O(k)
 */
